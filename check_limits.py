2be431723c18a84a8e447c063601ec475f0ccc5a
KevalBavadiya <60056875+KevalBavadiya@users.noreply.github.com>
2024-09-08 10:34:19 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,26 +1,34 @@
+TEMPERATURE_LIMITS = (0, 45)
+SOC_LIMITS = (20, 80)
+CHARGE_RATE_LIMITS = (0, 0.8)
 
-def is_temperature_ok(temperature):
-    if temperature < 0 or temperature > 45:
-        print('Temperature is out of range!')
-        return False
-    return True
+class BatteryParameter:
+    def __init__(self, name, value, limits):
+        self.name = name
+        self.value = value
+        self.lower_limit, self.upper_limit = limits
 
-def is_soc_ok(soc):
-    if soc < 20 or soc > 80:
-        print('State of Charge is out of range!')
-        return False
-    return True
+    def is_ok(self):
+        if self.value < self.lower_limit:
+            return False, f'{self.name} is too low!'
+        if self.value > self.upper_limit:
+            return False, f'{self.name} is too high!'
+        return True, ''
 
-def is_charge_rate_ok(charge_rate):
-    if charge_rate > 0.8:
-        print('Charge rate is out of range!')
-        return False
-    return True
+class Battery:
+    def __init__(self, temperature, soc, charge_rate):
+        self.temperature = BatteryParameter("Temperature", temperature, TEMPERATURE_LIMITS)
+        self.soc = BatteryParameter("State of Charge", soc, SOC_LIMITS)
+        self.charge_rate = BatteryParameter("Charge Rate", charge_rate, CHARGE_RATE_LIMITS)
 
-def battery_is_ok(temperature, soc, charge_rate):
-    return is_temperature_ok(temperature) and is_soc_ok(soc) and is_charge_rate_ok(charge_rate)
-
+    def is_battery_ok(self):
+        parameters = [self.temperature, self.soc, self.charge_rate]
+        for parameter in parameters:
+            is_ok, message = parameter.is_ok()
+            if not is_ok:
+                return False, message
+        return True, "Battery is OK"
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    assert(Battery(25, 70, 0.7).is_battery_ok()[0] is True)
+    assert(Battery(50, 85, 0).is_battery_ok()[0] is False)
